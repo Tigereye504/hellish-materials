cd034ff3f06c7a94363a7a9b7d8711ee2b2e6b70
David Troyer <dvdtroyer351@gmail.com>
2020-09-28 19:26:17 -0700

    fix Vuld armor values, update dependencies

--- src/main/java/net/tigereye/hellishmaterials/armor/VuldArmorMaterial.java
+++ src/main/java/net/tigereye/hellishmaterials/armor/VuldArmorMaterial.java
@@ -10,7 +10,7 @@ import net.minecraft.sound.SoundEvents;
 public class VuldArmorMaterial implements ArmorMaterial {
 
     private static final int[] BASE_DURABILITY = new int[] { 13, 15, 16, 11 };
-    private static final int[] PROTECTION_AMOUNTS = new int[] { 6, 16, 12, 6 };
+    private static final int[] PROTECTION_AMOUNTS = new int[] { 6, 12, 16, 6 };
 
     @Override
     public int getDurability(EquipmentSlot slot) {
