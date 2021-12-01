4d816a23dd3c9167c7fca643f4d058a8ccfc7d61
tigereye504 <dvdtroyer351@gmail.com>
2021-11-30 22:53:24 -0800

    minecraft 1.18 update

--- src/main/java/net/tigereye/hellishmaterials/HellishMaterials.java
+++ src/main/java/net/tigereye/hellishmaterials/HellishMaterials.java
@@ -7,7 +7,7 @@ import net.tigereye.hellishmaterials.registration.HMItems;
 public class HellishMaterials implements ModInitializer{
     
     public static final String MODID = "hellish-materials";
-    public static final boolean DEBUG = false;
+    public static final boolean DEBUG = true;
 
     @Override
     public void onInitialize() {
---
+++ src/main/java/net/tigereye/hellishmaterials/HellishMaterialsClient.java
@@ -0,0 +1,25 @@
+package net.tigereye.hellishmaterials;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.Identifier;
+import net.tigereye.hellishmaterials.items.Luckstone;
+import net.tigereye.hellishmaterials.registration.HMItems;
+
+public class HellishMaterialsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        FabricModelPredicateProviderRegistry.register(HMItems.LUCKSTONE, new Identifier("luck"), (itemStack, clientWorld, livingEntity, seed) -> {
+            float luck = 1;
+            NbtCompound tag = itemStack.getOrCreateNbt();
+            if (tag.contains(Luckstone.DISPLAY_KEY)) {
+                luck = tag.getFloat(Luckstone.DISPLAY_KEY);
+            }
+            if(HellishMaterials.DEBUG) {
+                System.out.println("Luckstone Diplay: " + luck);
+            }
+            return luck;
+        });
+    }
+}
